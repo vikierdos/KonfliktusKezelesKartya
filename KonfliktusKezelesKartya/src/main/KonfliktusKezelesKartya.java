caeef463f97eac0abc7d84045deaa95fb548171b
vikierdos <csicsorka@gmail.com>
2026-09-23 15:59:32 +0200

    keveres, case 1

--- KonfliktusKezelesKartya/src/main/KonfliktusKezelesKartya.java
+++ KonfliktusKezelesKartya/src/main/KonfliktusKezelesKartya.java
@@ -1,11 +1,27 @@
 package main;
 
-
 public class KonfliktusKezelesKartya {
-
+     
+    static String[] pakli = new String[22];
     
     public static void main(String[] args) {
         
+    }
+
+    private static void kever(int oszlop) {
+        String[] ujPakli = new String[22];
+        
+        switch(oszlop){
+            case 1 -> {
+                for (int i = 0; i <= 7; i++) {
+                    ujPakli[i] = pakli[20-(i-1)*3];
+                    ujPakli[i+7] = pakli[19-(i-1)*3];
+                    ujPakli[i+14] = pakli[21-(i-1)*3];
+                }
+            }
+        }
+        
+        pakli = ujPakli;
     }
     
 }
