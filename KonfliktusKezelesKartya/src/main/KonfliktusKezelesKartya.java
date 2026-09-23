9b9e82494010a78965e99a399353d01fa978fa0f
Kovács Tímea Barbara (SZF_N_2025_2027) <kovacs.timea.barbara2@diak.szamalk-szalezi.hu>
2026-09-23 15:58:33 +0200

    case 2, 3

--- KonfliktusKezelesKartya/src/main/KonfliktusKezelesKartya.java
+++ KonfliktusKezelesKartya/src/main/KonfliktusKezelesKartya.java
@@ -8,4 +8,23 @@ public class KonfliktusKezelesKartya {
         
     }
     
+    private static void kever(int oszlop) {
+        switch (oszlop) {
+            case 2->{
+                for (int i = 0; i < 7; i++) {
+                    ujPakli[i]= pakli[19-(i-1)*3];
+                    ujPakli[i+7]= pakli[20-(i-1)*3];
+                    ujPakli[i+14]= pakli[21-(i-1)*3];
+                }
+            }
+            case 3->{
+                for (int i = 0; i < 7; i++) {
+                    ujPakli[i]= pakli[19-(i-1)*3];
+                    ujPakli[i+7]= pakli[21-(i-1)*3];
+                    ujPakli[i+14]= pakli[20-(i-1)*3];
+                }
+            }
+        }
+    }
+    
 }
